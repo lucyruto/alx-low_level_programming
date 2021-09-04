a5b58a89b1321b89cf616b06871100d49930c500
lucyruto <lucyruto.lr@gmail.com>
2021-09-04 22:14:04 +0000

    Number combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
