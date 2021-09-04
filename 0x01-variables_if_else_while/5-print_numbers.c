cc37726585178aa5a7f18b62a7d72051c60734b2
lucyruto <lucyruto.lr@gmail.com>
2021-09-04 21:42:10 +0000

    Digits0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print numbers 0 through 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
