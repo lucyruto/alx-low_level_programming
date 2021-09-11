32218c69db9203a828380f9399fbe4047bdeaceb
lucyruto <lucyruto.lr@gmail.com>
2021-09-11 13:51:36 +0000

    edits

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
-/**
- * main prints _putchar, followed by a new line
- *
- * Return (0)
- */
-
-int main(void)
+/*
+*main- function to print
+*
+*Function return:(0)
+*/
+int main (void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
+	char _putchar[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	unsigned int c;
+	for (c = 0; c <sizeof(_putchar); c++)
 	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
-
+		_putchar(_putchar[c]);
+	}	
+	_putchar('\n');
 	return (0);
 }
